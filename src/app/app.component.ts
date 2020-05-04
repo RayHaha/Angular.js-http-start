37eeeacc5bca8132292eb5b87735cef621e1f036
RayHaha <g820324@gmail.com>
2020-05-04 15:32:32 -0400

    use type with HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -16,10 +17,10 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post(
+      .post<{ name: string }>(
         'https://ng-complete-guide-d26fc.firebaseio.com/posts.json',
         postData
       )
@@ -38,11 +39,12 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts(){
-    this.http.get('https://ng-complete-guide-d26fc.firebaseio.com/posts.json')
+    // get<type> is the same way to assign type at responseData: type but handle by Angular
+    this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-d26fc.firebaseio.com/posts.json')
     .pipe(
       map(
         responseData => {
-          const postsArray = [];
+          const postsArray: Post[] = [];
           for(const key in responseData){
             if(responseData.hasOwnProperty(key)){
               postsArray.push({ ...responseData[key], id: key });
