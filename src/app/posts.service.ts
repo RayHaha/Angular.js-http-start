9c10c73186d9f41663a539c02c075b544dfef3db
RayHaha <g820324@gmail.com>
2020-05-13 14:52:21 -0400

    use the catchError operator

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService{
@@ -41,7 +41,10 @@ export class PostsService{
           }
           return postsArray;
         }
-      )
+      ), catchError(errorRes => {
+          // send to analytics server or somethings you need to handle 
+          return throwError(errorRes);
+      })
     )
     }
 
