aefc7ce633515b786e194dcb81b12f52ec1ef259
RayHaha <g820324@gmail.com>
2020-05-17 22:04:05 -0400

    add query params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Post } from './post.model';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
@@ -28,27 +28,33 @@ export class PostsService{
     }
 
     fetchPosts(){
+        let searchParams = new HttpParams();
+        searchParams = searchParams.append('print','pretty');
+        searchParams = searchParams.append('custom','key');
+
         // get<type> is the same way to assign type at responseData: type but handle by Angular
-    return this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-d26fc.firebaseio.com/posts.json',
-    {
-        headers: new HttpHeaders({ 'Custom-Header': 'Hello' })
-    })
-    .pipe(
-      map(
-        responseData => {
-          const postsArray: Post[] = [];
-          for(const key in responseData){
-            if(responseData.hasOwnProperty(key)){
-              postsArray.push({ ...responseData[key], id: key });
+        return this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-d26fc.firebaseio.com/posts.json',
+        {
+            headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+            /*params: new HttpParams().set('print', 'pretty')*/
+            params: searchParams
+        })
+        .pipe(
+        map(
+            responseData => {
+            const postsArray: Post[] = [];
+            for(const key in responseData){
+                if(responseData.hasOwnProperty(key)){
+                postsArray.push({ ...responseData[key], id: key });
+                }
             }
-          }
-          return postsArray;
-        }
-      ), catchError(errorRes => {
-          // send to analytics server or somethings you need to handle 
-          return throwError(errorRes);
-      })
-    )
+            return postsArray;
+            }
+        ), catchError(errorRes => {
+            // send to analytics server or somethings you need to handle 
+            return throwError(errorRes);
+        })
+        )
     }
 
     deletePost(){
